3f04056da41e3cae76b730af537e75a30be97e51
Alexandre Luiz <alexandre.cerutti@live.com>
2022-07-26 20:07:33 -0300

    Corrections

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import  React  from 'react'
 import { PokemonCardsList } from "../components/cards";
 import { PageFooter } from '../components/footer';
-import { HomePageHeader } from '../components/home-page-header';
+import { HomePageHeader } from '../components/home';
 import { PokemonTypesCard } from '../components/pokemon-types';
 
 export const Home = () => {
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,6 +1,6 @@
 import  React  from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import ScrollToTop from '../components/scroll-to-top';
 import { Home } from "./home";
 import { PokemonPage } from './pokemon-info';
 
@@ -8,6 +8,7 @@ export const AppRoutes = () => {
 
     return(
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/pokemon/:id" element={<PokemonPage />} />
